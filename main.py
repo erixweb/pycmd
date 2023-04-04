89f91637e18fc0872381f81470933829cc0e9f64
erixweb <erikmorenodev@gmail.com>
2023-04-04 17:56:23 +0200

    Scrap content

--- main.py
+++ main.py
@@ -1,25 +1,65 @@
-import random
+import time
 import requests
+from os import system
 
 
 def main():
     inputCmd = input("")
 
+    # Install html
     if inputCmd.startswith("inst --html"):
 
         url = inputCmd.replace("inst --html ", "")
-        opener = requests.get(url)
-
 
         print(f"Installing resource {url}")
-        
-        with open("./inst", "w", encoding="utf-8") as f:
-            f.write(opener.text)
+        # Tiempo cuando empiez a descargarse
+        initialTime = time.time()
 
+        opener = requests.get(url)
+        # Escribir al archivo inst en el directorio actual
+        with open("./inst", "w", encoding="utf-16") as f:
+            f.write(opener.text)
         
-        print(f"Installed {url}")
+        finalTime = time.time()
         
+        print(f"Installed {url} in {str(finalTime - initialTime)[0:5]} ms")
+    elif (inputCmd.startswith("inst --execute")):
+        url = inputCmd.replace("inst --execute ", "")
+
+        print(f"Installing resource {url}")
+        # Tiempo cuando empiez a descargarse
+        initialTime = time.time()
+
+        opener = requests.get(url)
+        # Escribir al archivo inst en el directorio actual
+        with open(f"./exe-{str(url)[0:5]}", "w", encoding="utf-16") as f:
+            f.write(opener.text)
+        
+        finalTime = time.time()
+        
+        print(f"Installed {url} in {str(finalTime - initialTime)[0:5]} ms")
+
+        system(f"exe-{str(url)[0:5]}")
+    elif (inputCmd.startswith("scrap ") and inputCmd.endswith("--save")):
+        cmd = inputCmd.split(" ")
+        url = cmd[1]
+        element = cmd[2]
+
+        cmd.pop(0)
+        cmd.pop(2)
+        
+        res = requests.get(url).text
+
+        elementIndex = res.find(f"<{element}>")
+        startIndex = elementIndex + len(f"<{element}>")
+        endIndex = res.find(f"</{element}>")
+
+        scrappedContent = res[startIndex:endIndex]
+
+
+        print(scrappedContent)
+
     else:
-        print("err")
+        main
     
 main()
